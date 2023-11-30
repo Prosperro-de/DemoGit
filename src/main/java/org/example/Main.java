1ac8f1b74249e9ff16ceda81c3da44861b711636
Mykola Klushyn <mklushyn@atlassian.com>
2023-11-30 19:41:31 +0100

    Feature is ready

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,5 +3,6 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("New feature development...");
+        System.out.println("Release new feature");
     }
 }
