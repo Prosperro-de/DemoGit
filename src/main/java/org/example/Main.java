dd16aa219edd8417a36e57d4bdd5d1f3e17dfa00
Mykola Klushyn <mklushyn@atlassian.com>
2023-11-30 19:48:52 +0100

    Revert "Feature is ready"
    This reverts commit 1ac8f1b74249e9ff16ceda81c3da44861b711636.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,6 +3,5 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("New feature development...");
-        System.out.println("Release new feature");
     }
 }
