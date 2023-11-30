817a47b9557b8527a8d0aaf1cb677e1c64fb9529
Mykola Klushyn <mklushyn@atlassian.com>
2023-11-30 19:56:59 +0100

    create login form

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,5 +4,14 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("New feature development...");
         System.out.println("Release new feature");
+        login();
+    }
+
+    public static void login() {
+        String username = "Mykola";
+        String password = "qwerty";
+
+        System.out.println("username = " + username);
+        System.out.println("password = " + password);
     }
 }
